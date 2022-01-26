dbf2f269bda73287bf46d73e59527f34525f83d6
Niranjan <nirionline@gmail.com>
2022-01-26 13:18:18 +0530

    Version_1

--- Chatbot.py
+++ Chatbot.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-
+# comment for git
 app = Flask(__name__)
 english_bot = ChatBot("Chatterbot",storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ChatterBotCorpusTrainer(english_bot)
